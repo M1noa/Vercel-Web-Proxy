25d5ea7aff857434a00a15d6983027e7d368f5ec
Minoa <57844837+M1noa@users.noreply.github.com>
2023-11-02 13:44:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Fastify = require('fastify');
 const server = Fastify();
 
 server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://shuttleproxy.com/',
+  upstream: 'https://1v1.lol/',
   prefix: '/', 
   http2: false,
 });
