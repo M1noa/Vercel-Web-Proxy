3218b685fbf5e66907e01833b04fef26574a654d
Minoa <57844837+M1noa@users.noreply.github.com>
2023-11-02 11:42:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = Fastify();
 server.register(require('@fastify/http-proxy'), {
   upstream: 'https://discord.com/app',
   prefix: '/', 
-  http2: false,
+  http2: true,
 });
 
 server.listen({host: "0.0.0.0", port: 3000 });
