3a8b945406e0f902a60f2ea4a0a9d6acb227f399
Minoa <57844837+M1noa@users.noreply.github.com>
2023-11-02 09:06:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const Fastify = require('fastify');
 const server = Fastify();
 
 server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://shuttleproxy.com',
+  upstream: 'https://discord.com/app',
+  prefix: '/discord', 
+  http2: false,
+});
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://reddit.com',
+  prefix: '/reddit', 
+  http2: false,
+});
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://radon.games',
   prefix: '/', 
+  http2: false,
+});
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://schoolcheats.net/',
+  prefix: '/chez', 
   http2: false,
 });
 
