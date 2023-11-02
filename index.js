9a51e3f028397ab3b444fd4483cd6901f66756c0
Minoa <57844837+M1noa@users.noreply.github.com>
2023-11-02 11:44:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Fastify = require('fastify');
 const server = Fastify();
 
 server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://discord.com/app',
+  upstream: 'https://totallyscience.co/',
   prefix: '/', 
-  http2: true,
+  http2: false,
 });
 
 server.listen({host: "0.0.0.0", port: 3000 });
