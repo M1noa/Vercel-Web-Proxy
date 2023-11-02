03845f44413f611639ad768a21f03fa97f2931f2
Minoa <57844837+M1noa@users.noreply.github.com>
2023-11-02 15:49:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,27 @@ const server = Fastify();
 
 server.register(require('@fastify/http-proxy'), {
   upstream: 'https://1v1.lol/',
-  prefix: '/', 
+  prefix: '/1v1', 
   http2: false,
 });
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://https://shellshock.io/',
+  prefix: '/shellshock', 
+  http2: false,
+});
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://skribbl.io/
+  prefix: '/skribbl', 
+  http2: false,
+});
+
+// Serve the facts.html file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 
 server.listen({host: "0.0.0.0", port: 3000 });
 
