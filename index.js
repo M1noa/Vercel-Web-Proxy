6c624e94247fa503bd6812db45eef09813ff31cb
Minoa <57844837+M1noa@users.noreply.github.com>
2023-11-02 13:34:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Fastify = require('fastify');
 const server = Fastify();
 
 server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://artclass.site/gs.html',
+  upstream: 'https://artclass.site/',
   prefix: '/', 
   http2: false,
 });
