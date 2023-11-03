6e7406fed54a394a7c06b534f83edd7b6883f3d3
Minoa <57844837+M1noa@users.noreply.github.com>
2023-11-03 11:06:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 const Fastify = require('fastify');
 const server = Fastify();
 
+// Proxys
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://shuttleproxy.com/',
+  prefix: '/prox/', 
+  http2: false,
+});
+
+// Chat Sites
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://discord.com/app',
+  prefix: '/discord/', 
+  http2: false,
+});
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://omegle.onl/online/',
+  prefix: '/omegle/', 
+  http2: false,
+});
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://monkey.app/',
+  prefix: '/monkey/', 
+  http2: false,
+});
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://ome.tv/',
+  prefix: '/ome/', 
+  http2: false,
+});
+
+// Games
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://now.gg/apps/roblox-corporation/5349/roblox.html',
+  prefix: '/roblox/', 
+  http2: false,
+});
+
+server.register(require('@fastify/http-proxy'), {
+  upstream: 'https://nativegames.net/',
+  prefix: '/native/', 
+  http2: false,
+});
+
 server.register(require('@fastify/http-proxy'), {
   upstream: 'https://1v1.lol/',
   prefix: '/1v1/', 
@@ -16,60 +61,6 @@ server.register(require('@fastify/http-proxy'), {
 server.register(require('@fastify/http-proxy'), {
   upstream: 'https://skribbl.io/',
   prefix: '/skribbl/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://shuttleproxy.com/',
-  prefix: '/prox/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://discord.com/',
-  prefix: '/discord/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://omegle.onl/home/',
-  prefix: '/omegle/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://now.gg/apps/roblox-corporation/5349/roblox.html',
-  prefix: '/roblox/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://nativegames.net/',
-  prefix: '/native/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://music.apple.com/us/browse',
-  prefix: '/music/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://spotify.com/',
-  prefix: '/spotify/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://youtube.com/',
-  prefix: '/youtube/', 
-  http2: false,
-});
-
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://tiktok.com/',
-  prefix: '/tiktok/', 
   http2: false,
 });
 
