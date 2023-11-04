89f05347964d3415c08001b8ffb7f2e1a49b2264
Minoa <57844837+M1noa@users.noreply.github.com>
2023-11-03 20:47:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,10 +46,4 @@ server.register(require('@fastify/http-proxy'), {
   http2: false,
 });
 
-// Movies
-server.register(require('@fastify/http-proxy'), {
-  upstream: 'https://shuttletv.net/',
-  prefix: '/tv/', 
-  http2: false,
-});
 server.listen({host: "0.0.0.0", port: 3000 });
